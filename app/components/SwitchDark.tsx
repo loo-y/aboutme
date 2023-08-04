dcd8af5497860a9bf49889b0264329d318bd560d
Loo <loo@Mac-mini-M2.lan>
2023-08-05 01:33:07 +0800

    add dark mode switch button

---
+++ app/components/SwitchDark.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useState } from 'react'
+interface SwitchDarkProps {
+    initSwitch?: boolean
+    callback?: (isDark: boolean) => void
+}
+const SwitchDark = ({ initSwitch = false, callback }: SwitchDarkProps) => {
+    console.log(`initSwitch`, initSwitch)
+    const [isSwitch, setIsSwitch] = useState(initSwitch)
+    const handleSwitch = () => {
+        setIsSwitch(!isSwitch)
+        if (callback) callback(!isSwitch)
+    }
+
+    return (
+        <div
+            className={`w-14 h-7 bg-slate-400 bg-opacity-50  rounded-2xl  py-0.5 px-[0.2rem] flex relative cursor-pointer ease-linear duration-500 ${
+                isSwitch ? 'shadow-slate-300/50 shadow-md' : 'shadow-slate-500/50 shadow-lg'
+            }`}
+            onClick={handleSwitch}
+        >
+            {isSwitch ? (
+                <div className="w-6 h-6 bg-slate-800 rounded-full p-1 ease-linear duration-300 ml-[1.65rem]">
+                    <img src="./misc/moon.svg" alt="dark" />
+                </div>
+            ) : (
+                <div className="w-6 h-6 bg-gray-100 rounded-full p-1 ease-linear duration-300 ml-0">
+                    <img src="./misc/sun.svg" alt="light" />
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default SwitchDark
--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -1,20 +1,19 @@
 'use client'
-
 import { createContext, useState } from 'react'
-
+import SwitchDark from './components/SwitchDark'
 export const ThemeContext = createContext({ dark: false })
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
     const [dark, setDark] = useState(false)
     return (
         <ThemeContext.Provider value={{ dark: dark }}>
-            <div
-                className="w-16 h-5 fixed right-0 top-0 bg-transparent cursor-pointer text-transparent"
-                onClick={() => {
-                    setDark(!dark)
-                }}
-            >
-                switch
+            <div className="fixed right-3 top-2 cursor-pointer z-[999]">
+                <SwitchDark
+                    initSwitch={dark}
+                    callback={isDark => {
+                        setDark(isDark)
+                    }}
+                />
             </div>
             <div className={`${dark ? 'dark bg-slate-800' : ''}`}>{children}</div>
         </ThemeContext.Provider>
